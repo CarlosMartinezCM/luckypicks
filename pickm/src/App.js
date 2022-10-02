0d60e4cf74dc3b53fb0faf7058a7e0a9ed214f4a
carlos m <j.car.mart.86@gmail.com>
2022-10-01 17:20:14 -0700

    small change to home page

--- pickm/src/App.js
+++ pickm/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Muscle memory trying to flex.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
