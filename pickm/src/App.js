168fa625193bf4eeea4d4b89508ceb12d941b3b8
carlos <https://github.com/CarlosMartinezCM>
2022-10-05 07:43:25 -0700

    minor text changes

--- pickm/src/App.js
+++ pickm/src/App.js
@@ -7,12 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code> import brains from head </code> 
         </p>
         <p>
           Muscle memory trying to flex.<br></br>
-          time to upload(push) the changes I made to my github on the wBranch.
+          time to upload(push) the changes I made to my github on the wBranch.<br></br>
           Now I will commit changes to branch wBranch to my git repo!!
+        </p>
+        <p>
+          Next is creating another page and then APIs while updating the wBranch. <code>10/05/2022.</code>
         </p>
         <a
           className="App-link"
